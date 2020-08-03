9751fc38fc3b913b5075ccd4241b219e92c49349
ziugs@hotmail.com <ziugs@hotmail.com>
2020-08-03 15:52:19 +0300

    converted to feet and knots

--- src/main/java/ee/minutiandmed/ziugs/controllers/MainController.java
+++ src/main/java/ee/minutiandmed/ziugs/controllers/MainController.java
@@ -3,6 +3,7 @@ package ee.minutiandmed.ziugs.controllers;
 import ee.minutiandmed.ziugs.Downloader;
 import ee.minutiandmed.ziugs.DownloaderData;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -18,7 +20,7 @@ import java.time.format.DateTimeFormatter;
 @Controller
 public class MainController {
 
-    private static final String[] STATION_SARRAY = {"jogeva", "johvi", "kihnu", "kunda", "kuusiku", "laanenigula",
+    private static final String[] STATIONS_ARRAY = {"jogeva", "johvi", "kihnu", "kunda", "kuusiku", "laanenigula",
             "narva", "parnu", "ristna", "ruhnu", "sorve", "harku", "toravere", "mustvee", "turi", "vaikemaarja",
             "viljandi", "vilsandi", "voru"};
 
@@ -32,21 +34,30 @@ public class MainController {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM");
         String formattedDate = date.format(dateFormat);
         DownloaderData data = downloader.getData();
+        String na = "N/A";
 
         LocalTime lt = LocalTime.parse(data.getUpdateTimeOnServer().get(0));
-        model.addAttribute("time", String.format("Hetkel on kaardil %s %s UTC andmed", formattedDate, timeFormat.format(lt.plusMinutes(3))));
+        model.addAttribute("time", String.format("Hetkel on kaardil %s %s UTC andmed", formattedDate, timeFormat.format(lt.plusMinutes(4))));
 
-        for (int i = 0; i < STATION_SARRAY.length; i++) {
-            if (data.getCloudBase().get(i).isBlank()) {
-                model.addAttribute(STATION_SARRAY[i], String.format("N/A %s km <br/> %s° %s m/s <br/> %s",
-                        data.getVisibility().get(i),
-                        data.getWindDirection().get(i), data.getWindSpeed().get(i),
-                        data.getWeatherFenomenon().get(i)));
 
+        for (int i = 0; i < STATIONS_ARRAY.length; i++) {
+            long convertedToFeet = Math.round(NumberUtils.toInt(data.getCloudBase().get(i)) * 3.3);
+            float convertedToKnots = 0;
+            if (data.getWindSpeed().get(i).isEmpty()) {
+                data.getWindSpeed().set(i, "0");
             } else {
-                model.addAttribute(STATION_SARRAY[i], String.format("%s m %s %s km <br/> %s° %s m/s <br/> %s",
-                        data.getCloudBase().get(i), data.getOkta().get(i), data.getVisibility().get(i),
-                        data.getWindDirection().get(i), data.getWindSpeed().get(i),
+                String str = data.getWindSpeed().get(i).replace(",", ".");
+                convertedToKnots = (float) Math.round(Float.parseFloat(str) * 1.94);
+            }
+            DecimalFormat df = new DecimalFormat("###");
+            if (data.getCloudBase().get(i).isEmpty()) {
+                model.addAttribute(STATIONS_ARRAY[i], String.format("N/A %s km <br/> %s° %s kt <br/> %s",
+                        data.getVisibility().get(i), data.getWindDirection().get(i), df.format(convertedToKnots),
+                        data.getWeatherFenomenon().get(i)));
+            } else {
+                model.addAttribute(STATIONS_ARRAY[i], String.format("%d ft %s %s km <br/> %s° %s kt <br/> %s",
+                        convertedToFeet, data.getOkta().get(i), data.getVisibility().get(i),
+                        data.getWindDirection().get(i), df.format(convertedToKnots),
                         data.getWeatherFenomenon().get(i)));
             }
         }
